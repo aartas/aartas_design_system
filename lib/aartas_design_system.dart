5554d2f44fc794c427ffd678d0c47c3dcc623761
Prathamesh Kumbhar <itsmepk@Prathameshs-Mac-mini.local>
2022-07-06 12:49:22 +0530

    Add Demo

--- lib/aartas_design_system.dart
+++ lib/aartas_design_system.dart
@@ -1,7 +1,18 @@
 library aartas_design_system;
 
+import 'package:flutter/material.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+class DemoWidget extends StatelessWidget {
+  const DemoWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Your Demo is working");
+  }
 }
