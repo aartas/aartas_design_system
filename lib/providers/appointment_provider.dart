d206a9626de33b5c926c103c5f803396dea2ce28
Prathamesh Kumbhar <itsmepk@Prathameshs-Mac-mini.local>
2022-07-20 17:31:34 +0530

    update

--- lib/providers/appointment_provider.dart
+++ lib/providers/appointment_provider.dart
@@ -3,12 +3,12 @@ import 'package:aartas_design_system/models/appointment_model.dart';
 import 'package:flutter/material.dart';
 
 class AppointmentProvider extends ChangeNotifier {
-  AppointmentResponse _result = AppointmentResponse();
+  AppointmentResponse response = AppointmentResponse();
   bool loading = false;
 
   getResponse() async {
     loading = true;
-    _result = await AppointmentApis().getAppointments();
+    response = await AppointmentApis().getAppointments();
     loading = false;
     notifyListeners();
   }
