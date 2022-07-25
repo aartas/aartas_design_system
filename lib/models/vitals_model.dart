d7e207fef80d0f22f756879239b6d9f1c3d8b6da
Prathamesh Kumbhar <itsmepk@Prathameshs-Mac-mini.local>
2022-07-25 21:25:50 +0530

    update

--- lib/models/vitals_model.dart
+++ lib/models/vitals_model.dart
@@ -1,3 +1,27 @@
+class VitalsResponse {
+  String? message;
+  bool? status;
+  Vitals? data;
+
+  VitalsResponse({this.message, this.status, this.data});
+
+  VitalsResponse.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    status = json['status'];
+    data = json['data'] != null ? Vitals.fromJson(json['data']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['message'] = message;
+    data['status'] = status;
+    if (this.data != null) {
+      data['data'] = this.data!.toJson();
+    }
+    return data;
+  }
+}
+
 class Vitals {
   int? id;
   dynamic appointmentId;
