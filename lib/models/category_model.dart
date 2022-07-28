3eb0f6778b57f0e5315f5e991b36626fd38fc58e
Prathamesh Kumbhar <itsmepk@Prathameshs-Mac-mini.local>
2022-07-28 15:40:21 +0530

    “update”

---
+++ lib/models/category_model.dart
@@ -0,0 +1,82 @@
+class CategoryResponse {
+  String? message;
+  bool? status;
+  List<CategoryData>? data;
+
+  CategoryResponse({this.message, this.status, this.data});
+
+  CategoryResponse.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    status = json['status'];
+    if (json['data'] != null) {
+      data = <CategoryData>[];
+      json['data'].forEach((v) {
+        data!.add(CategoryData.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['message'] = message;
+    data['status'] = status;
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class CategoryData {
+  int? id;
+  String? title;
+  List<SubCategory>? subCategory;
+
+  CategoryData({this.id, this.title, this.subCategory});
+
+  CategoryData.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    if (json['sub_category'] != null) {
+      subCategory = <SubCategory>[];
+      json['sub_category'].forEach((v) {
+        subCategory!.add(SubCategory.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['title'] = title;
+    if (subCategory != null) {
+      data['sub_category'] = subCategory!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class SubCategory {
+  int? id;
+  int? categoryId;
+  int? inputType;
+  String? title;
+
+  SubCategory({this.id, this.categoryId, this.inputType, this.title});
+
+  SubCategory.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    categoryId = json['category_id'];
+    inputType = json['input_type'];
+    title = json['title'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['category_id'] = categoryId;
+    data['input_type'] = inputType;
+    data['title'] = title;
+    return data;
+  }
+}
