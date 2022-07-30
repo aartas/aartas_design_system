6b212c99b345113bd1b604a1da672e1be6b3e0ba
RapidQuote <rapidquote2021@gmail.com>
2022-07-30 23:17:25 +0530

    “update”

--- lib/providers/medicine_provider.dart
+++ lib/providers/medicine_provider.dart
@@ -1,3 +1,51 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:developer';
 
-class MedicineProvider extends ChangeNotifier {}
+import 'package:aartas_design_system/const.dart';
+import 'package:aartas_design_system/models/medicine_model.dart';
+import 'package:aartas_design_system/models/medicine_unit_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MedicineProvider extends ChangeNotifier {
+  Future<MedicineUnitResponse> fetchUnitList() async {
+    var _url = Uri.parse("$baseURL/medicine/unit/list");
+    final res = await http.post(_url);
+
+    String _message = "(${res.statusCode}) $_url";
+    log(_message);
+    if (res.statusCode == 200) {
+      return MedicineUnitResponse.fromJson(json.decode(res.body));
+    } else {
+      log(res.body);
+      return MedicineUnitResponse(message: _message);
+    }
+  }
+
+  Future<MedicineResponse> fetchMedicineList(
+    String? doctorID,
+    String? specialityID,
+    String? search,
+    String? limit,
+    String? offset,
+  ) async {
+    var _url = Uri.parse("$baseURL/medicine/list");
+    final res = await http.post(_url, body: {
+      "doctor_id": doctorID ?? "",
+      "speciality_id": specialityID ?? "",
+      "search": search ?? "",
+      "limit": limit ?? "",
+      "offset": offset ?? "",
+    });
+
+    String _message =
+        "(${res.statusCode}) $_url: doctorID:$doctorID, specialityID:$specialityID, search:$search, limit:$limit, offset:$offset";
+    log(_message);
+    if (res.statusCode == 200) {
+      return MedicineResponse.fromJson(json.decode(res.body));
+    } else {
+      log(res.body);
+      return MedicineResponse(message: _message);
+    }
+  }
+}
