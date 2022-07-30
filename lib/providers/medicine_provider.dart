5d10d036b68f27210fc6b9210cbee8c4c51ca726
RapidQuote <rapidquote2021@gmail.com>
2022-07-30 23:31:29 +0530

    “update”

--- lib/providers/medicine_provider.dart
+++ lib/providers/medicine_provider.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 import 'package:aartas_design_system/const.dart';
 import 'package:aartas_design_system/models/medicine_model.dart';
 import 'package:aartas_design_system/models/medicine_unit_model.dart';
+import 'package:aartas_design_system/models/response_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -46,6 +47,50 @@ class MedicineProvider extends ChangeNotifier {
     } else {
       log(res.body);
       return MedicineResponse(message: _message);
+    }
+  }
+
+  Future<ResponseModel> saveMedicine(
+    String? appointmentID,
+    String? medicineID,
+    String? dose,
+    String? unitID,
+    String? qty,
+    String? frequency,
+    String? duration,
+    String? durationType,
+    String? startDate,
+    String? instructions,
+    String? timeOfTheDayDefaults,
+    String? timeOfTheDay,
+    String? timeRange,
+    String? oldID,
+  ) async {
+    var _url = Uri.parse("$baseURL/save/appointment/medicine");
+    final res = await http.post(_url, body: {
+      "appointment_id": appointmentID ?? "",
+      "medicine_id": medicineID ?? "",
+      "dose": dose ?? "",
+      "unit_id": unitID ?? "",
+      "qty": qty ?? "",
+      "frequency": frequency ?? "",
+      "duration": duration ?? "",
+      "duration_type": durationType ?? "",
+      "start_date": startDate ?? "",
+      "instructions": instructions ?? "",
+      "time_of_the_day_defaults": timeOfTheDayDefaults ?? "",
+      "time_of_the_day": timeOfTheDay ?? "",
+      "time_range": timeRange ?? "",
+      "old_id": oldID ?? ""
+    });
+
+    String _message = "(${res.statusCode}) $_url";
+    log(_message);
+    if (res.statusCode == 200) {
+      return ResponseModel.fromJson(json.decode(res.body));
+    } else {
+      log(res.body);
+      return ResponseModel(message: _message);
     }
   }
 }
