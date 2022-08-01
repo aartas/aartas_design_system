3fdf31bfd4e0aa95a30ca120588c8fb8207a66a8
Prathamesh Kumbhar <itsmepk@Prathameshs-Mac-mini.local>
2022-08-01 14:10:17 +0530

    “update”

--- lib/providers/appointment_provider.dart
+++ lib/providers/appointment_provider.dart
@@ -55,4 +55,33 @@ class AppointmentProvider extends ChangeNotifier {
 
     return AppointmentResponse(message: _message);
   }
+
+  Future<AppointmentResponse> fetchAppointmentData(
+    String? appointmentID,
+    String? doctorID,
+    String? search,
+    String? date,
+    String? limit,
+    String? offset,
+    String? type,
+    bool? manageState,
+  ) async {
+    var _url = Uri.parse("$baseURL/appointment/details");
+    final res = await http.post(_url, body: {
+      "appointment_id": appointmentID ?? "",
+    });
+    String _message = "(${res.statusCode}) $_url";
+    log(_message);
+    if (res.statusCode == 200) {
+      final _res = AppointmentResponse.fromJson(json.decode(res.body));
+      if (manageState == null || manageState == true) {
+        _list.clear();
+        _list = _res.data!;
+        notifyListeners();
+      }
+      return _res;
+    }
+
+    return AppointmentResponse(message: _message);
+  }
 }
