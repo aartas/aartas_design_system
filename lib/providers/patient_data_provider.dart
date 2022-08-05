af63ac763f9f0b7fa74672da5d3503d70bd880f8
Prathamesh Kumbhar <itsmepk@Prathameshs-Mac-mini.local>
2022-08-05 13:04:44 +0530

    “update”

--- lib/providers/patient_data_provider.dart
+++ lib/providers/patient_data_provider.dart
@@ -6,22 +6,27 @@ class PatientDataProvider with ChangeNotifier {
   PatientData _patientData = PatientData();
   // final List<PatientData> _patientList = [];
 
-  // PatientData getData() {
-  //   return _patientData;
-  // }
+  PatientData getData() {
+    return _patientData;
+  }
 
-  // _setData(PatientData val) {
-  //   _patientData = val;
-  //   notifyListeners();
-  // }
+  _setData(PatientData val) {
+    _patientData = val;
+    notifyListeners();
+  }
 
-  // Future<PatientData> fetchData(String? phoneNumber) {
-  //   return AuthenticationProvider().patientLogin(phoneNumber!).then((value) {
-  //     _setData(value.data![0]);
-  //     notifyListeners();
-  //     return value.data![0];
-  //   });
-  // }
+  Future<PatientData> fetchData(
+    String baseURL,
+    String? phoneNumber,
+  ) {
+    return AuthenticationProvider()
+        .patientLogin(baseURL, phoneNumber!)
+        .then((value) {
+      _setData(value.data![0]);
+      notifyListeners();
+      return value.data![0];
+    });
+  }
 
   // Future<List<PatientData>> fetchData(){
   //   return
