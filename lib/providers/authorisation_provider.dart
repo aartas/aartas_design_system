3a65dc89c8f8712caa772c148854392c1e796d4a
Prathamesh Kumbhar <itsmepk@Prathameshs-Mac-mini.local>
2022-08-09 13:01:45 +0530

    “update”

--- lib/providers/authorisation_provider.dart
+++ lib/providers/authorisation_provider.dart
@@ -28,4 +28,38 @@ class AuthorisationProvider with ChangeNotifier {
       return ResponseModel(message: _message);
     }
   }
+
+  Future<ResponseModel> updateFcmToken(
+    String baseURL,
+    String? userID,
+    String? token,
+    String? location,
+    String? latitude,
+    String? longitude,
+    String? platform,
+    String? version,
+  ) async {
+    var _url = Uri.parse("$baseURL/patient/update/fcm/token");
+    if (token != null && token != "") {
+      final res = await http.post(_url, body: {
+        "user_id": userID,
+        "fcm_token": token,
+        "location": location,
+        "latitude": latitude,
+        "longitude": longitude,
+        "version": version,
+        "platform": platform,
+      });
+      if (res.statusCode == 200) {
+        return ResponseModel.fromJson(json.decode(res.body));
+      } else {
+        String _message = "Update FCM Token:${res.statusCode}";
+        log(_message);
+        return ResponseModel(message: _message);
+      }
+    } else {
+      String _message = "FCM Token is null!";
+      return ResponseModel(message: _message);
+    }
+  }
 }
