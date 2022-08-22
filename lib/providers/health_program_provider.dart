b46acc86a1989cdcfbfd3ce01e121f2258179ebd
Prathamesh Kumbhar <itsmepk@Prathameshs-Mac-mini.local>
2022-08-22 20:59:28 +0530

    Update PatientProvider

---
+++ lib/providers/health_program_provider.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:aartas_design_system/models/health_program_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HealthProgramProvider with ChangeNotifier {
+  List<BundleList> _healthProgramList = [];
+
+  List<BundleList> getList() {
+    return _healthProgramList;
+  }
+
+  // final List<PatientData> _patientList = [];
+
+  Future<HealthProgramResponse> fetchList(
+    String baseURL,
+    String? patientID,
+    bool? manageState,
+  ) async {
+    var _url = Uri.parse("$baseURL/patient/list");
+    final res = await http.post(_url, body: {
+      "patient_id": patientID ?? "",
+    });
+
+    String _message = "(${res.statusCode}) $_url:";
+    log(_message);
+    if (res.statusCode == 200) {
+      final _res = HealthProgramResponse.fromJson(json.decode(res.body));
+      if (manageState == null || manageState == true) {
+        _healthProgramList.clear();
+        _healthProgramList = _res.data!.bundleList!;
+        notifyListeners();
+      }
+      return _res;
+    } else {
+      log(res.body);
+      return HealthProgramResponse(message: _message);
+    }
+  }
+}
