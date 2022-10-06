3200acf924495cff8ec12fe1013a4a7a532adca3
Prathamesh Kumbhar <itsmepk@Prathameshs-Mac-mini.local>
2022-10-06 16:15:20 +0530

    Modified

---
+++ lib/models/trending_page_model.dart
@@ -0,0 +1,80 @@
+import 'package:aartas_design_system/models/doctor_model.dart';
+import 'package:aartas_design_system/models/home_feed_model.dart';
+
+class TrendingResponse {
+  String? message;
+  bool? status;
+  TrendingData? data;
+
+  TrendingResponse({this.message, this.status, this.data});
+
+  TrendingResponse.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    status = json['status'];
+    data = json['data'] != null ? TrendingData.fromJson(json['data']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['message'] = message;
+    data['status'] = status;
+    if (this.data != null) {
+      data['data'] = this.data!.toJson();
+    }
+    return data;
+  }
+}
+
+class TrendingData {
+  List<HomeFeed>? homeFeedData;
+  List<DoctorData>? doctors;
+  List<HomeFeed>? testimonials;
+  String? homeFeedImageUrl;
+  String? doctorImageUrl;
+
+  TrendingData(
+      {this.homeFeedData,
+      this.doctors,
+      this.testimonials,
+      this.homeFeedImageUrl,
+      this.doctorImageUrl});
+
+  TrendingData.fromJson(Map<String, dynamic> json) {
+    if (json['home_feed_data'] != null) {
+      homeFeedData = <HomeFeed>[];
+      json['home_feed_data'].forEach((v) {
+        homeFeedData!.add(HomeFeed.fromJson(v));
+      });
+    }
+    if (json['doctors'] != null) {
+      doctors = <DoctorData>[];
+      json['doctors'].forEach((v) {
+        doctors!.add(DoctorData.fromJson(v));
+      });
+    }
+    if (json['testimonials'] != null) {
+      testimonials = <HomeFeed>[];
+      json['testimonials'].forEach((v) {
+        testimonials!.add(HomeFeed.fromJson(v));
+      });
+    }
+    homeFeedImageUrl = json['home_feed_image_url'];
+    doctorImageUrl = json['doctor_image_url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (homeFeedData != null) {
+      data['home_feed_data'] = homeFeedData!.map((v) => v.toJson()).toList();
+    }
+    if (doctors != null) {
+      data['doctors'] = doctors!.map((v) => v.toJson()).toList();
+    }
+    if (testimonials != null) {
+      data['testimonials'] = testimonials!.map((v) => v.toJson()).toList();
+    }
+    data['home_feed_image_url'] = homeFeedImageUrl;
+    data['doctor_image_url'] = doctorImageUrl;
+    return data;
+  }
+}
---
+++ lib/models/version_model.dart
@@ -0,0 +1,71 @@
+class Version {
+  String? message;
+  bool? status;
+  VersionData? data;
+
+  Version({this.message, this.status, this.data});
+
+  Version.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    status = json['status'];
+    data = json['data'] != null ? VersionData.fromJson(json['data']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['message'] = message;
+    data['status'] = status;
+    if (this.data != null) {
+      data['data'] = this.data!.toJson();
+    }
+    return data;
+  }
+}
+
+class VersionData {
+  List<VersionList>? versionList;
+
+  VersionData({this.versionList});
+
+  VersionData.fromJson(Map<String, dynamic> json) {
+    if (json['version_list'] != null) {
+      versionList = <VersionList>[];
+      json['version_list'].forEach((v) {
+        versionList!.add(VersionList.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (versionList != null) {
+      data['version_list'] = versionList!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class VersionList {
+  int? id;
+  String? title;
+  String? value;
+  int? status;
+
+  VersionList({this.id, this.title, this.value, this.status});
+
+  VersionList.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    value = json['value'];
+    status = json['status'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['title'] = title;
+    data['value'] = value;
+    data['status'] = status;
+    return data;
+  }
+}
