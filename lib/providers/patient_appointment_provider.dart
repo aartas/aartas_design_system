e4bef78ef02f4e732e2122881b6191700e553d63
Prathamesh Kumbhar <itsmepk@Prathameshs-Mac-mini.local>
2022-10-19 13:22:49 +0530

    Modified

--- lib/providers/patient_appointment_provider.dart
+++ lib/providers/patient_appointment_provider.dart
@@ -1,231 +1,231 @@
-// import 'dart:convert';
-// import 'dart:developer';
-// import 'package:aartas_design_system/models/confirm_appointment_model.dart';
-// import 'package:aartas_design_system/models/patient_ahead_model.dart';
-// import 'package:aartas_design_system/models/patient_appointment_list_model.dart';
+import 'dart:convert';
+import 'dart:developer';
+import 'package:aartas_design_system/models/confirm_appointment_model.dart';
+import 'package:aartas_design_system/models/patient_ahead_model.dart';
+import 'package:aartas_design_system/models/patient_appointment_list_model.dart';
 
-// import 'package:aartas_design_system/models/unconfirmed_appointment_model.dart';
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
+import 'package:aartas_design_system/models/unconfirmed_appointment_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-// class PatientAppointmentProvider extends ChangeNotifier {
-//   List<PatientAppointmentData> _list = [];
-//   final List<PatientAppointmentData> _filteredList = [];
+class PatientAppointmentProvider extends ChangeNotifier {
+  List<PatientAppointmentData> _list = [];
+  final List<PatientAppointmentData> _filteredList = [];
 
-//   PatientAppointmentData? _data;
+  PatientAppointmentData? _data;
 
-//   bool _isLoading = false;
-//   bool isLoading() {
-//     return _isLoading;
-//   }
+  bool _isLoading = false;
+  bool isLoading() {
+    return _isLoading;
+  }
 
-//   PatientAppointmentData getData() {
-//     return _data!;
-//   }
+  PatientAppointmentData getData() {
+    return _data!;
+  }
 
-//   List<PatientAppointmentData> getList() {
-//     return _list;
-//   }
+  List<PatientAppointmentData> getList() {
+    return _list;
+  }
 
-//   List<PatientAppointmentData> getFilteredList() {
-//     return _filteredList;
-//   }
+  List<PatientAppointmentData> getFilteredList() {
+    return _filteredList;
+  }
 
-//   Future<PatientAppointmentResponse> fetchData(
-//     String baseUrl,
-//     String? appointmentID,
-//   ) async {
-//     _isLoading = true;
-//     var _url = Uri.parse("$baseUrl/appointment/$appointmentID");
-//     final res = await http.get(_url);
-//     String _message = "(${res.statusCode}) $_url";
-//     log(_message);
-//     if (res.statusCode == 200) {
-//       final _res = PatientAppointmentResponse.fromJson(
-//         json.decode(res.body),
-//       );
-//       _data = _res.data!.first;
-//       _isLoading = false;
-//       notifyListeners();
-//       return _res;
-//     } else {
-//       _isLoading = false;
-//       notifyListeners();
-//       log(res.body);
-//       return PatientAppointmentResponse(message: _message);
-//     }
-//   }
+  Future<PatientAppointmentResponse> fetchData(
+    String baseUrl,
+    String? appointmentID,
+  ) async {
+    _isLoading = true;
+    var _url = Uri.parse("$baseUrl/appointment/$appointmentID");
+    final res = await http.get(_url);
+    String _message = "(${res.statusCode}) $_url";
+    log(_message);
+    if (res.statusCode == 200) {
+      final _res = PatientAppointmentResponse.fromJson(
+        json.decode(res.body),
+      );
+      _data = _res.data!.first;
+      _isLoading = false;
+      notifyListeners();
+      return _res;
+    } else {
+      _isLoading = false;
+      notifyListeners();
+      log(res.body);
+      return PatientAppointmentResponse(message: _message);
+    }
+  }
 
-//   Future<PatientAppointmentResponse> fetchList(
-//     String baseUrl,
-//     String? patientID,
-//     String? doctorID,
-//     String? search,
-//     String? date,
-//     String? limit,
-//     String? offset,
-//     String? type,
-//     bool? manageState,
-//   ) async {
-//     _isLoading = true;
-//     notifyListeners();
-//     var _url = Uri.parse("$baseUrl/patient/all/appointments");
-//     final res = await http.post(_url, body: {
-//       "patient_id": patientID ?? "",
-//       "doctor_id": doctorID ?? "",
-//       "search": search ?? "",
-//       "date": date ?? "",
-//       "limit": limit ?? "",
-//       "offset": offset ?? "",
-//       "type": type ?? "",
-//     });
-//     String _message = "(${res.statusCode}) $_url";
-//     log(_message);
-//     if (res.statusCode == 200) {
-//       final _res = PatientAppointmentResponse.fromJson(
-//         json.decode(res.body),
-//       );
-//       if (manageState == null || manageState == true) {
-//         _list.clear();
-//         _list = _res.data!;
-//         _isLoading = false;
-//         notifyListeners();
-//       }
-//       return _res;
-//     } else {
-//       _isLoading = false;
-//       notifyListeners();
-//       log(res.body);
-//       return PatientAppointmentResponse(message: _message);
-//     }
-//   }
+  Future<PatientAppointmentResponse> fetchList(
+    String baseUrl,
+    String? patientID,
+    String? doctorID,
+    String? search,
+    String? date,
+    String? limit,
+    String? offset,
+    String? type,
+    bool? manageState,
+  ) async {
+    _isLoading = true;
+    notifyListeners();
+    var _url = Uri.parse("$baseUrl/patient/all/appointments");
+    final res = await http.post(_url, body: {
+      "patient_id": patientID ?? "",
+      "doctor_id": doctorID ?? "",
+      "search": search ?? "",
+      "date": date ?? "",
+      "limit": limit ?? "",
+      "offset": offset ?? "",
+      "type": type ?? "",
+    });
+    String _message = "(${res.statusCode}) $_url";
+    log(_message);
+    if (res.statusCode == 200) {
+      final _res = PatientAppointmentResponse.fromJson(
+        json.decode(res.body),
+      );
+      if (manageState == null || manageState == true) {
+        _list.clear();
+        _list = _res.data!;
+        _isLoading = false;
+        notifyListeners();
+      }
+      return _res;
+    } else {
+      _isLoading = false;
+      notifyListeners();
+      log(res.body);
+      return PatientAppointmentResponse(message: _message);
+    }
+  }
 
-//   updateFilterAppointments(int _selected) {
-//     _filteredList.clear();
-//     if (_selected == 0) {
-//       // Upcoming
-//       for (var i = 0; i < _list.length; i++) {
-//         if (_list[i].appointmentStatus != 1 &&
-//             _list[i].appointmentStatus != 3 &&
-//             _list[i].appointmentStatus != 8 &&
-//             _list[i].appointmentStatus != 9) {
-//           _filteredList.add(_list[i]);
-//           notifyListeners();
-//         }
-//       }
-//     }
-//     if (_selected == 1) {
-//       // Completed , Payment Done, Cancelled
-//       for (var i = 0; i < _list.length; i++) {
-//         if (_list[i].appointmentStatus == 8 ||
-//             _list[i].appointmentStatus == 9 ||
-//             _list[i].appointmentStatus == 3) {
-//           _filteredList.add(_list[i]);
-//           notifyListeners();
-//         }
-//       }
-//     }
-//   }
+  updateFilterAppointments(int _selected) {
+    _filteredList.clear();
+    if (_selected == 0) {
+      // Upcoming
+      for (var i = 0; i < _list.length; i++) {
+        if (_list[i].appointmentStatus != 1 &&
+            _list[i].appointmentStatus != 3 &&
+            _list[i].appointmentStatus != 8 &&
+            _list[i].appointmentStatus != 9) {
+          _filteredList.add(_list[i]);
+          notifyListeners();
+        }
+      }
+    }
+    if (_selected == 1) {
+      // Completed , Payment Done, Cancelled
+      for (var i = 0; i < _list.length; i++) {
+        if (_list[i].appointmentStatus == 8 ||
+            _list[i].appointmentStatus == 9 ||
+            _list[i].appointmentStatus == 3) {
+          _filteredList.add(_list[i]);
+          notifyListeners();
+        }
+      }
+    }
+  }
 
-//   Future<UnconfirmedAppointment> unconfirmedAppointment(
-//     String? baseURL,
-//     String? patientId,
-//     String? doctorId,
-//     String? slotId,
-//   ) async {
-//     var _url = Uri.parse("$baseURL/book/appointment");
-//     log("Unconfirm Appointment: $patientId, $doctorId, $slotId");
-//     final res = (await http.post(_url, body: {
-//       "doctor_id": doctorId,
-//       "patient_id": patientId,
-//       "timeslot_id": slotId
-//     }))
-//         .body;
-//     notifyListeners();
-//     log("Network:${json.decode(res)['message']}");
-//     return UnconfirmedAppointment.fromJson(json.decode(res));
-//   }
+  Future<UnconfirmedAppointment> unconfirmedAppointment(
+    String? baseURL,
+    String? patientId,
+    String? doctorId,
+    String? slotId,
+  ) async {
+    var _url = Uri.parse("$baseURL/book/appointment");
+    log("Unconfirm Appointment: $patientId, $doctorId, $slotId");
+    final res = (await http.post(_url, body: {
+      "doctor_id": doctorId,
+      "patient_id": patientId,
+      "timeslot_id": slotId
+    }))
+        .body;
+    notifyListeners();
+    log("Network:${json.decode(res)['message']}");
+    return UnconfirmedAppointment.fromJson(json.decode(res));
+  }
 
-//   Future<ConfirmAppointment> confirmedAppointment(
-//     String? baseURL,
-//     String? appointmentId,
-//     String? couponId,
-//     String? bundleId,
-//     String? rewardAmount,
-//   ) async {
-//     var _url = Uri.parse("$baseURL/confirm/appointment");
-//     // print("BUNDLE ID: $bundleId");
-//     final res = (await http.post(_url, body: {
-//       "appointment_id": appointmentId,
-//       "coupon_id": couponId,
-//       "bundle_id": bundleId != 'null' ? bundleId : "",
-//       "reward_points": rewardAmount != "null" ? rewardAmount : ""
-//     }))
-//         .body;
+  Future<ConfirmAppointment> confirmedAppointment(
+    String? baseURL,
+    String? appointmentId,
+    String? couponId,
+    String? bundleId,
+    String? rewardAmount,
+  ) async {
+    var _url = Uri.parse("$baseURL/confirm/appointment");
+    // print("BUNDLE ID: $bundleId");
+    final res = (await http.post(_url, body: {
+      "appointment_id": appointmentId,
+      "coupon_id": couponId,
+      "bundle_id": bundleId != 'null' ? bundleId : "",
+      "reward_points": rewardAmount != "null" ? rewardAmount : ""
+    }))
+        .body;
 
-//     log("Network:$res");
-//     notifyListeners();
-//     return ConfirmAppointment.fromJson(json.decode(res));
-//   }
+    log("Network:$res");
+    notifyListeners();
+    return ConfirmAppointment.fromJson(json.decode(res));
+  }
 
-//   Future rescheduleAppointment(
-//     String? baseURL,
-//     String? appointmentId,
-//     String? timeslotId,
-//   ) async {
-//     var _url = Uri.parse("$baseURL/reschedule/appointment");
-//     final res = (await http.post(
-//       _url,
-//       body: {
-//         "appointment_id": appointmentId,
-//         "timeslot_id": timeslotId,
-//       },
-//     ))
-//         .body;
-//     notifyListeners();
-//     log("Network:${json.decode(res)['message']}");
-//     return json.decode(res);
-//   }
+  Future rescheduleAppointment(
+    String? baseURL,
+    String? appointmentId,
+    String? timeslotId,
+  ) async {
+    var _url = Uri.parse("$baseURL/reschedule/appointment");
+    final res = (await http.post(
+      _url,
+      body: {
+        "appointment_id": appointmentId,
+        "timeslot_id": timeslotId,
+      },
+    ))
+        .body;
+    notifyListeners();
+    log("Network:${json.decode(res)['message']}");
+    return json.decode(res);
+  }
 
-//   Future cancelAppointment(
-//     String? baseURL,
-//     String? appointmentId,
-//   ) async {
-//     var _url = Uri.parse("$baseURL/cancel/appointment");
-//     final res = (await http.post(
-//       _url,
-//       body: {
-//         "appointment_id": appointmentId ?? "",
-//       },
-//     ))
-//         .body;
-//     notifyListeners();
-//     log("Network:${json.decode(res)['message']}");
-//     return json.decode(res);
-//   }
+  Future cancelAppointment(
+    String? baseURL,
+    String? appointmentId,
+  ) async {
+    var _url = Uri.parse("$baseURL/cancel/appointment");
+    final res = (await http.post(
+      _url,
+      body: {
+        "appointment_id": appointmentId ?? "",
+      },
+    ))
+        .body;
+    notifyListeners();
+    log("Network:${json.decode(res)['message']}");
+    return json.decode(res);
+  }
 
-//   Future<PatientAheadResponse> getPatientsAhead(
-//     String baseUrl,
-//     String? appointmentID,
-//   ) async {
-//     _isLoading = true;
-//     notifyListeners();
-//     var _url = Uri.parse("$baseUrl/appointment/get/patient/ahead");
-//     final res = await http.post(_url, body: {
-//       "appointment_id": appointmentID ?? "",
-//     });
-//     String _message = "(${res.statusCode}) $_url:";
-//     log(_message);
-//     if (res.statusCode == 200) {
-//       final _res = PatientAheadResponse.fromJson(json.decode(res.body));
-//       _isLoading = false;
-//       notifyListeners();
-//       return _res;
-//     } else {
-//       _isLoading = false;
-//       notifyListeners();
-//       log(res.body);
-//       return PatientAheadResponse(message: _message);
-//     }
-//   }
-// }
+  Future<PatientAheadResponse> getPatientsAhead(
+    String baseUrl,
+    String? appointmentID,
+  ) async {
+    _isLoading = true;
+    notifyListeners();
+    var _url = Uri.parse("$baseUrl/appointment/get/patient/ahead");
+    final res = await http.post(_url, body: {
+      "appointment_id": appointmentID ?? "",
+    });
+    String _message = "(${res.statusCode}) $_url:";
+    log(_message);
+    if (res.statusCode == 200) {
+      final _res = PatientAheadResponse.fromJson(json.decode(res.body));
+      _isLoading = false;
+      notifyListeners();
+      return _res;
+    } else {
+      _isLoading = false;
+      notifyListeners();
+      log(res.body);
+      return PatientAheadResponse(message: _message);
+    }
+  }
+}
