60835bf03baecb1f8158d52e34e83a9eefd50eea
Prathamesh Kumbhar <itsmepk@Prathameshs-Mac-mini.local>
2022-12-26 19:52:55 +0530

    Update AartasLogo and assets to pubspec

---
+++ lib/src/aartas_logo.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AartasLogo extends StatelessWidget {
+  final double? size;
+  final Color? color;
+  const AartasLogo({
+    Key? key,
+    this.size,
+    this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image(
+      image: const AssetImage("assets/AartasLogo.png"),
+      height: size ?? 20,
+      fit: BoxFit.contain,
+      color: color,
+      alignment: Alignment.bottomCenter,
+    );
+  }
+}
