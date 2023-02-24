342b668f1614b2941629246589c64038db0d6ff0
Prathamesh Kumbhar <itsmepk@Prathameshs-Mac-mini.local>
2023-02-24 09:33:36 +0530

    Remove json.decode(res.body)['status'] from fetchDartDetails

--- lib/providers/patient_cart_provider.dart
+++ lib/providers/patient_cart_provider.dart
@@ -25,7 +25,7 @@ class PatientCartProvider extends ChangeNotifier {
     String _message = "(${res.statusCode}) $_url";
     log(_message);
 
-    if (res.statusCode == 200 && json.decode(res.body)['status']) {
+    if (res.statusCode == 200) {
       var _res = PatientCartResponse.fromJson(json.decode(res.body));
       _data = _res.data!;
       notifyListeners();
