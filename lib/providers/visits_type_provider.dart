3a111184524f6a245c9300867ce79bc3f452bcda
Prathamesh <tech@Prathameshs-MacBook-Pro.local>
2023-06-02 17:59:46 +0530

    Add register in VisitPurposeProvider

--- lib/providers/visits_type_provider.dart
+++ lib/providers/visits_type_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:developer';
 
+import 'package:aartas_design_system/models/response_model.dart';
 import 'package:aartas_design_system/models/visits_model.dart';
 import 'package:flutter/material.dart';
 
@@ -27,6 +28,31 @@ class VisitPurposeProvider extends ChangeNotifier {
       notifyListeners();
       return _res;
     } else {
+      return null;
+    }
+  }
+
+  Future<ResponseModel?> register(
+    String? baseURL,
+    String? fullName,
+    String? phoneNumber,
+  ) async {
+    var url = Uri.parse("$baseURL/user/register");
+
+    final res = await http.post(
+      url,
+      body: {
+        "full_name": fullName,
+        "phone_number": phoneNumber ?? "",
+      },
+    );
+
+    log("(${res.statusCode}) $url");
+    if (res.statusCode == 200) {
+      notifyListeners();
+      return ResponseModel.fromJson(json.decode(res.body));
+    } else {
+      log(res.body);
       return null;
     }
   }
