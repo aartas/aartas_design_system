8e3a512f06416e6cce9e274e46f68db814d1096b
Prathamesh <tech@Prathameshs-MacBook-Pro.local>
2023-06-14 16:56:08 +0530

    add sendWhatsappLink KioskAppProvider

--- lib/kiosk/providers/kiosk_app_provider.dart
+++ lib/kiosk/providers/kiosk_app_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:aartas_design_system/kiosk/models/kiosk_response_model.dart';
+import 'package:aartas_design_system/models/response_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -25,6 +26,29 @@ class KioskApiProvider extends ChangeNotifier {
     if (res.statusCode == 200) {
       var _res = KioskAppResponse.fromJson(json.decode(res.body));
       data = _res.data!;
+      notifyListeners();
+      return _res;
+    } else {
+      return null;
+    }
+  }
+
+  Future<ResponseModel?> sendWhatsappLink(
+    String? baseURL,
+    String? userId,
+    String? purpose,
+  ) async {
+    var url = Uri.parse("$baseURL/user/send/whatsapp/link");
+
+    final res = await http.post(url, body: {
+      "user_id": userId ?? "",
+      "purpose": purpose ?? "",
+    });
+
+    log("(${res.statusCode}) $url");
+
+    if (res.statusCode == 200) {
+      var _res = ResponseModel.fromJson(json.decode(res.body));
       notifyListeners();
       return _res;
     } else {
